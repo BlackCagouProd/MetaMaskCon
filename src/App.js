4ff6fce6e7a9039c34ec0c19feecb70237043787
BlackCagou <blackcagou@outlook.fr>
2022-04-02 05:03:35 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,163 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import { Contract, providers } from "ethers";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isWalletInstalled, setIsWalletInstalled] = useState(false);
+    const [date, setDate] = useState("1990-03-31");
+    // state for zodiacSign derived from date.
+    const [zodiacSign, setZodiacSign] = useState(null);
+
+    // state for keeping track of current connected account.
+    const [account, setAccount] = useState(null);
+
+    // state for whether app is minting or not.
+    const [isMinting, setIsMinting] = useState(false);
+
+    const [NFTContract, setNFTContract] = useState(null);
+
+    useEffect(() => {
+        calculateZodiacSign(date);
+    }, [date]);
+
+    useEffect(() => {
+        if (window.ethereum) {
+            setIsWalletInstalled(true);
+        }
+    }, []);
+
+    async function connectWallet() {
+        window.ethereum
+            .request({
+                method: "eth_requestAccounts",
+            })
+            .then((accounts) => {
+                setAccount(accounts[0]);
+            })
+            .catch((error) => {
+                alert("Something went wrong");
+            });
+    }
+
+    function handleDateInput({ target }) {
+        setDate(target.value);
+    }
+
+    function calculateZodiacSign(date) {
+        let dateObject = new Date(date);
+        let day = dateObject.getDate();
+        let month = dateObject.getMonth();
+        if (month == 0) {
+            if (day >= 20) {
+                setZodiacSign("Aquarius");
+            } else {
+                setZodiacSign("Capricorn");
+            }
+        } else if (month == 1) {
+            if (day >= 19) {
+                setZodiacSign("Pisces");
+            } else {
+                setZodiacSign("Aquarius");
+            }
+        } else if (month == 2) {
+            if (day >= 21) {
+                setZodiacSign("Aries");
+            } else {
+                setZodiacSign("Pisces");
+            }
+        } else if (month == 3) {
+            if (day >= 20) {
+                setZodiacSign("Taurus");
+            } else {
+                setZodiacSign("Aries");
+            }
+        } else if (month == 4) {
+            if (day >= 21) {
+                setZodiacSign("Gemini");
+            } else {
+                setZodiacSign("Taurus");
+            }
+        } else if (month == 5) {
+            if (day >= 21) {
+                setZodiacSign("Cancer");
+            } else {
+                setZodiacSign("Gemini");
+            }
+        } else if (month == 6) {
+            if (day >= 23) {
+                setZodiacSign("Leo");
+            } else {
+                setZodiacSign("Cancer");
+            }
+        } else if (month == 7) {
+            if (day >= 23) {
+                setZodiacSign("Virgo");
+            } else {
+                setZodiacSign("Leo");
+            }
+        } else if (month == 8) {
+            if (day >= 23) {
+                setZodiacSign("Libra");
+            } else {
+                setZodiacSign("Virgo");
+            }
+        } else if (month == 9) {
+            if (day >= 23) {
+                setZodiacSign("Scorpio");
+            } else {
+                setZodiacSign("Libra");
+            }
+        } else if (month == 10) {
+            if (day >= 22) {
+                setZodiacSign("Sagittarius");
+            } else {
+                setZodiacSign("Scorpio");
+            }
+        } else if (month == 11) {
+            if (day >= 22) {
+                setZodiacSign("Capricorn");
+            } else {
+                setZodiacSign("Sagittarius");
+            }
+        }
+    }
+
+
+
+    async function connectWallet() {
+        window.ethereum
+            .request({
+                method: "eth_requestAccounts",
+            })
+            .then((accounts) => {
+                setAccount(accounts[0]);
+            })
+            .catch((error) => {
+                alert("Something went wrong");
+            });
+    }
+    if (account === null) {
+        return ( <
+            div className = "App" > {
+                isWalletInstalled ? ( <
+                    button onClick = { connectWallet } > Connect Wallet < /button>
+                ) : ( <
+                    p > Install Metamask wallet < /p>
+                )
+            }
+
+            <
+            /div>
+        );
+    }
+    return ( <
+        div className = "App" >
+        <
+        p > Connected as: { account } < /p> < /
+        div >
+    );
 }
+
 
 export default App;
